8c8df665495d8658338a26cc6f5dc2ab17b4076d
AlvaroMorte <alvaromorte8888@gmail.com>
2020-12-02 22:10:26 +0200

    Add method "private"

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,11 +4,16 @@ class ArticlesController < ApplicationController
     
   end
 
-  def create
-    @article = Article.new(params[:article])
-
-    @article.save
-    redirect_to @article
+  def create # сохраняет статью в БД
+    @article = Article.new(article_params)#Rails может быть инициализирована с помощью соответствующих атрибутов, 
+    #которые будут автоматически привязаны к соответствующим столбцам базы данных. params[:article]содержит интересующие нас атрибуты
+    
+    @article.save #ответственен за сохранение модели в базу данных.
+    redirect_to @article  
   end
-
+  
+  private
+    def article_params
+      params.require(:article).permit(:titel, :text)
+    end  
 end
