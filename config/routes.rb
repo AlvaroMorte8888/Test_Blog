ad461f987ae0645f917121422fe134034fc8c94b
AlvaroMorte <alvaromorte8888@gmail.com>
2020-12-08 13:26:30 +0200

    Add routes delete

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   get "/articles", to: "articles#index"
   get 'welcome/index'
   root 'welcome#index'
+  delete  '/article/[:id]', to: "articles#destroy"
   resources :articles
 end
