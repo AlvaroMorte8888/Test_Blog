ce707e3c9361dea186038b4ffa4ce17ade8fab38
AlvaroMorte <alvaromorte8888@gmail.com>
2020-12-11 18:39:28 +0200

    Update model article.rb and add migrate.

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,12 @@
 class Article < ApplicationRecord
   has_many :comments #Добавить другую сторону связи
-  validates :title, presence: true,
-            length: {minimum: 5}
+  validates :title, presence: true, length: {minimum: 5}
+
+  VALID_STATUSES = ['public', 'private', 'archived']
+
+  validates :status, in: VALID_STATUSES
+
+  def archived?
+    status == 'archived'
+  end
 end
