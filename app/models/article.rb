ffe0917a3ae5a4958514322842aa127410ac951d
AlvaroMorte <alvaromorte8888@gmail.com>
2020-12-12 12:11:38 +0200

    Delete comments

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,11 +1,7 @@
 class Article < ApplicationRecord
   has_many :comments #Добавить другую сторону связи
   validates :title, presence: true, length: {minimum: 5}
-
-  VALID_STATUSES = ['public', 'private', 'archived']
-
-  validates :status, in: VALID_STATUSES
-
+  
   def archived?
     status == 'archived'
   end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,10 +1,6 @@
 class Comment < ApplicationRecord
   belongs_to :article
 
-  VALID_STATUSES = ['public', 'private', 'archived']
-
-  validates :status, in: VALID_STATUSES
-
   def archived?
     status == 'archived'
   end
